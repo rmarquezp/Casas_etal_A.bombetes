103b7088a5ab95d314c91c2746046a0b72107195
Roberto Márquez <30904843+rmarquezp@users.noreply.github.com>
2017-12-28 11:28:32 -0600

    Commented out plotting functions

--- ColorAnalyses.R
+++ ColorAnalyses.R
@@ -41,40 +41,38 @@ write.csv(cbind(data,scores),file="ColorationOutputs.csv", quote=F,row.names=F)
 
 ##Plot
 
-colPick=function(x){
+#colPick=function(x){
+#	
+#	if(x=="FrogRed"){return("red")}
+#	if(x=="FrogYellow"){return("yellow")}	
+#	if(x=="ModelControl"){return("brown")}
+#	if(x=="ModelRed"){return("red")}
+#	if(x=="ModelYellow"){return("yellow")}
+#	}
+
+#symPick=function(x){
 	
-	if(x=="FrogControl"){return("brown")}
-	if(x=="FrogRed"){return("red")}
-	if(x=="FrogYellow"){return("yellow")}
+#	if(x=="FrogControl"){return(22)}
+#	if(x=="FrogRed"){return(22)}
+#	if(x=="FrogYellow"){return(22)}
 	
-	if(x=="ModelControl"){return("brown")}
-	if(x=="ModelRed"){return("red")}
-	if(x=="ModelYellow"){return("yellow")}
-	}
+#	if(x=="ModelControl"){return(24)}
+#	if(x=="ModelRed"){return(24)}
+#	if(x=="ModelYellow"){return(24)}
+#	}
 
-symPick=function(x){
-	
-	if(x=="FrogControl"){return(22)}
-	if(x=="FrogRed"){return(22)}
-	if(x=="FrogYellow"){return(22)}
-	
-	if(x=="ModelControl"){return(24)}
-	if(x=="ModelRed"){return(24)}
-	if(x=="ModelYellow"){return(24)}
-	}
-
-cols=as.character(sapply(data$Morph,colPick))
-syms=unlist(sapply(data$Morph,symPick))
+#cols=as.character(sapply(data$Morph,colPick))
+#syms=unlist(sapply(data$Morph,symPick))
 
 
-par(mfrow=c(1,2))
+#par(mfrow=c(1,2))
 
-plot(scores[,1:2],pch=syms,bg=cols,xlim=c(-2.5,2.5),ylim=c(-2.5,2.5))
+#plot(scores[,1:2],pch=syms,bg=cols,xlim=c(-2.5,2.5),ylim=c(-2.5,2.5))
 
-plot(data$B,data$LM,pch=syms,bg=cols,xlab="B",ylab="LM",xlim=c(0.05,0.65),ylim=c(0.05,0.65))
+#plot(data$B,data$LM,pch=syms,bg=cols,xlab="B",ylab="LM",xlim=c(0.05,0.65),ylim=c(0.05,0.65))
 
 
-par(mfrow=c(1,2))
+#par(mfrow=c(1,2))
 
-barplot(pca$rotation[,1],border=NA,col=c("red","green","blue"),ylab="Loading",ylim=c(-0.6,0.7), main=names[1], cex.lab=1.5)
-barplot(pca$rotation[,2],border=NA,col=c("red","green","blue"),ylim=c(-0.6,0.7), main=names[2],cex.lab=1.5)
+#barplot(pca$rotation[,1],border=NA,col=c("red","green","blue"),ylab="Loading",ylim=c(-0.6,0.7), main=names[1], cex.lab=1.5)
+#barplot(pca$rotation[,2],border=NA,col=c("red","green","blue"),ylim=c(-0.6,0.7), main=names[2],cex.lab=1.5)
